f7876db03440c11cee6ba810f7edb08207f4b8f6
Tóth Bence 🐻 <toth.bence@simonyi.bme.hu>
2023-12-20 19:55:23 +0100

    done

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { combineLatest, map, of, tap } from 'rxjs';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  mergedCars$ = combineLatest([this.dataService.getNewCars$(), this.dataService.getUsedCars$()]).pipe(
+    tap(([newCars, usedCars]) => console.log(newCars, usedCars)),
+    map(([newCars, usedCars]) => {
+      const newCarsUsed = newCars.map((car: any) => ({ ...car, isUsed: false }));
+      const usedCarsUsed = usedCars.map((car: any) => ({ ...car, isUsed: true }));
+      return [newCarsUsed, usedCarsUsed];
+    }),
+    map(([newCars, usedCars]) => [...newCars, ...usedCars]),
+    map(cars => cars.sort((a, b) => a.year - b.year))
+  )
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.dataService.test();
   }
 
 }
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -8,10 +8,10 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class RegistrationComponent implements OnInit {
   userAddForm: FormGroup = new FormGroup({
-    name: new FormControl(''),
-    address: new FormControl(''),
-    country: new FormControl(''),
-    subscription: new FormControl(true)
+    name: new FormControl('', [Validators.required, this.customNameValidator()]),
+    address: new FormControl('', [Validators.required]),
+    country: new FormControl('', [Validators.required]),
+    subscription: new FormControl(true, [Validators.required])
   });
 
   constructor() { }
@@ -21,6 +21,30 @@ export class RegistrationComponent implements OnInit {
 
   submit() {
     console.log(this.userAddForm);
+  }
+
+  customNameValidator(): ValidatorFn {
+    return (control: AbstractControl) => {
+      const hasSpace = control.value.includes(' ');
+      const min3Char = control.value.split(' ').every((namePart: string) => namePart.length >= 3);
+      return hasSpace && min3Char ? null : { 'customNameInvalid': true };
+    }
+  }
+
+  get name() {
+    return this.userAddForm.get('name');
+  }
+
+  get address() {
+    return this.userAddForm.get('address');
+  }
+
+  get country() {
+    return this.userAddForm.get('country');
+  }
+
+  get subscription() {
+    return this.userAddForm.get('subscription');
   }
 
 }
